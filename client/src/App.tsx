5429ef5814d61c3ff0850a9a3b17ec2e2c0290dd
WiegnerO <30533050+WiegnerO@users.noreply.github.com>
2024-11-24 20:15:30 -0800

    GG-3 Addressed Pr comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,25 @@
-import { useEffect } from 'react'
-import './App.css'
-import { Button } from '@nextui-org/react'
+import { useEffect } from "react";
+import "./App.css";
+import { Button } from "@nextui-org/react";
 
 function App() {
-
   useEffect(() => {
-    fetch("/api/hello").then((r) => {
-      console.log(`API Status was: ${r.status}`)
-      return r.text()
-    }).then(t => console.log(`Backend says "${t}" (staus) `))
-  },[])
+    fetch("/api/hello")
+      .then((r) => {
+        console.log(`API Status was: ${r.status}`);
+        return r.text();
+      })
+      .then((t) => console.log(`Backend says "${t}" (staus) `));
+  }, []);
 
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Button color="primary">
-      Button
-    </Button>
-    </div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button color="primary">Button</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
